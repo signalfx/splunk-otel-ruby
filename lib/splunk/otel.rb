12781c59571f9a157608e68311c477850381db62
Tristan Sloughter <tsloughter+work@splunk.com>
2022-01-18 14:17:14 -0700

    test processor and exporter configuration

--- lib/splunk/otel.rb
+++ lib/splunk/otel.rb
@@ -15,44 +15,18 @@ module Splunk
         c.add_span_processor(OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(exporter))
       end
 
-      span_limits = OpenTelemetry::SDK::Trace::SpanLimits.new(attribute_count_limit: 1024,
-                                                              event_count_limit: 512,
-                                                              link_count_limit: 1000,
-                                                              event_attribute_count_limit: 256,
-                                                              link_attribute_count_limit: 128)
-
-      OpenTelemetry.tracer_provider.span_limits = span_limits
+      OpenTelemetry.tracer_provider.span_limits = gdi_span_limits
     end
 
-    module_function :configure
-    # def configure(tracer: nil,
-    #               ingest_url: ENV['SIGNALFX_ENDPOINT_URL'] || ENV['SIGNALFX_INGEST_URL'] || 'http://localhost:9080/v1/trace',
-    #               service_name: ENV['SIGNALFX_SERVICE_NAME'] || "signalfx-ruby-tracing",
-    #               access_token: ENV['SIGNALFX_ACCESS_TOKEN'],
-    #               auto_instrument: false,
-    #               span_tags: {})
-    #   @ingest_url = ingest_url
-    #   @service_name = service_name
-    #   @access_token = access_token
+    def gdi_span_limits
+      OpenTelemetry::SDK::Trace::SpanLimits.new(attribute_count_limit: 1024,
+                                                attribute_length_limit: 12_000,
+                                                event_count_limit: Float::INFINITY,
+                                                link_count_limit: Float::INFINITY,
+                                                event_attribute_count_limit: Float::INFINITY,
+                                                link_attribute_count_limit: Float::INFINITY)
+    end
 
-    #   span_tags = process_span_tags(span_tags)
-    #   set_tracer(tracer: tracer, service_name: service_name, access_token: access_token,
-    #              span_tags: span_tags) if @tracer.nil?
-
-    #   if auto_instrument
-    #     Register.available_libs.each_pair do |key, value|
-    #       begin
-    #         value.instrument
-    #       rescue Exception => e
-    #         logger.error { "failed to initialize instrumentation '#{key}': #{e.inspect}" }
-    #         logger.error { e.backtrace }
-    #       end
-    #     end
-    #   else
-    #     yield self
-    #   end
-
-    #   Compat.apply
-    # end
+    module_function :configure, :gdi_span_limits
   end
 end
--- test/splunk/otel_test.rb
+++ test/splunk/otel_test.rb
@@ -6,19 +6,49 @@ require "opentelemetry/exporter/otlp"
 
 module Splunk
   class OtelTest < Test::Unit::TestCase
-    test "configure SDK and start trace" do
-      Splunk::Otel.configure
+    def setup
+      with_env("OTEL_SERVICE_NAME" => "test-service") do
+        Splunk::Otel.configure
+      end
+    end
 
+    def teardown
+      OpenTelemetry.tracer_provider.shutdown
+    end
+
+    test "configure SDK and start trace" do
       assert_equal([
                      OpenTelemetry::Trace::Propagation::TraceContext.text_map_propagator,
                      OpenTelemetry::Baggage::Propagation.text_map_propagator
                    ], OpenTelemetry.propagation.instance_variable_get(:@propagators))
 
-      tracer = OpenTelemetry.tracer_provider.tracer("splunk-test", "1.0")
+      tracer_provider = OpenTelemetry.tracer_provider
+      tracer = tracer_provider.tracer("splunk-test", "1.0")
 
       tracer.in_span("span-1", attributes: { "attr.key.1" => "attr-value-1" }) do |span|
         span.set_attribute("attr.key.2", "attr-value-2")
       end
+
+      processors = OpenTelemetry.tracer_provider.instance_variable_get(:@span_processors)
+
+      batch_processor = processors[0]
+      assert_equal(512, batch_processor.instance_variable_get(:@batch_size))
+
+      exporter = batch_processor.instance_variable_get(:@exporter)
+      assert_equal("gzip", exporter.instance_variable_get(:@compression))
+    end
+
+    test "GDI defaults" do
+      span_limits = OpenTelemetry.tracer_provider.instance_variable_get(:@span_limits)
+
+      assert_equal(Float::INFINITY, span_limits.event_count_limit)
+      assert_equal(Float::INFINITY, span_limits.link_count_limit)
+    end
+
+    test "set service name" do
+      provider_resource = OpenTelemetry.tracer_provider.instance_variable_get(:@resource)
+      resource_attributes = provider_resource.instance_variable_get(:@attributes)
+      assert_equal("test-service", resource_attributes["service.name"])
     end
   end
 end
