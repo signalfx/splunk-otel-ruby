93d53eebae81f3810faaff08ee94c74c74436b9e
Tristan Sloughter <tsloughter+work@splunk.com>
2022-05-17 15:00:41 -0600

    add railtie based RUM Rack instrumentation

---
+++ lib/splunk/otel/instrumentation/action_pack/railtie.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Splunk
+  module Otel
+    module ActionPack
+      # Install the Rack middleware for RUM responses
+      class Railtie < ::Rails::Railtie
+        config.before_initialize do |app|
+          OpenTelemetry::Instrumentation::Rack::Instrumentation.instance.install({})
+
+          app.middleware.insert_before(
+            0,
+            Splunk::Otel::Rack::RumMiddleware
+          )
+        end
+      end
+    end
+  end
+end
