ff09580124c18d193569791c4a63943ef0d6cb97
Tristan Sloughter <tsloughter+work@splunk.com>
2022-07-15 13:14:38 -0600

    only insert RumMiddleware before ETag middleware on Rails 7

--- lib/splunk/otel/instrumentation/action_pack/railtie.rb
+++ lib/splunk/otel/instrumentation/action_pack/railtie.rb
@@ -9,10 +9,16 @@ module Splunk
         # Install the Rack middleware for RUM responses
         class Railtie < ::Rails::Railtie
           config.before_initialize do |app|
-            app.middleware.insert_before(
-              ::Rack::ETag,
-              Splunk::Otel::Rack::RumMiddleware
-            )
+            case Rails.version
+            when /^7\./
+              # TODO: can be removed once https://github.com/rails/rails/issues/45607 is merged
+              app.middleware.insert_before(
+                ::Rack::ETag,
+                Splunk::Otel::Rack::RumMiddleware
+              )
+            else
+              app.middleware.use Splunk::Otel::Rack::RumMiddleware
+            end
           end
         end
       end
