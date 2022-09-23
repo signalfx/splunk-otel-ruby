239132987db77a49c6640bce4ea01fff5c466384
Jakub Malinowski <jakubmal+github@gmail.com>
2022-09-23 14:35:12 +0200

    chore: pin dependencies

--- scripts/install-release-deps.sh
+++ scripts/install-release-deps.sh
@@ -7,4 +7,9 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg -
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 apt-get update
-apt-get -y install ca-certificates curl gnupg lsb-release gh
+apt-get -y install \
+  ca-certificates="20211016~20.04.1" \
+  curl="7.68.0-1ubuntu2.13" \
+  gnupg="2.2.19-3ubuntu2.2" \
+  lsb-release="11.1.0ubuntu2" \
+  gh="2.16.0"
