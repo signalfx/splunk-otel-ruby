17f50400bc61fb17f01ab000d3132b30c366237d
tsloughter-splunk <93154828+tsloughter-splunk@users.noreply.github.com>
2022-09-29 12:41:30 -0600

    pin debian packages in releaser to bullseye versions (#66)

--- scripts/install-release-deps.sh
+++ scripts/install-release-deps.sh
@@ -2,14 +2,17 @@
 
 set -e
 
+# ensure certs, curl and gpg are available before running them to setup the github repo
+apt-get update
+apt-get -y install \
+  ca-certificates="20210119" \
+  curl="7.74.0-1.3+deb11u3" \
+  gnupg="2.2.27-2+deb11u2" \
+  lsb-release="11.1.0" \
+
 # instructions from https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-apt
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 apt-get update
-apt-get -y install \
-  ca-certificates="20211016~20.04.1" \
-  curl="7.68.0-1ubuntu2.13" \
-  gnupg="2.2.19-3ubuntu2.2" \
-  lsb-release="11.1.0ubuntu2" \
-  gh="2.16.0"
+apt-get -y install gh="2.16.1"
