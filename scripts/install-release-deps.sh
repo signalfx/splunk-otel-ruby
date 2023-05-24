3df18689b2d201a5cc6c271b09db455aea332031
Siim Kallas <siimkallas@gmail.com>
2023-05-24 19:35:09 +0300

    chore: dont specify gh version for release deps (#162)

--- scripts/install-release-deps.sh
+++ scripts/install-release-deps.sh
@@ -15,4 +15,4 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg -
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 apt-get update
-apt-get -y install gh="2.16.1"
+apt-get -y install gh
